ca6f564c124c5be91ef820eede2b172c2e2476f8
Camron Stuart <Samlennder@gmail.com>
2019-07-18 13:01:04 -0400

    remove excess comments

--- database/cassandra.js
+++ database/cassandra.js
@@ -1,14 +1,3 @@
-// const cassandra = require('cassandra-driver')
-
-
-// const contact = '127.0.0.1:9042'
-
-// const cassClient = new cassandra.Client({
-//   contactPoints: ['127.0.0.1'],
-//   localDataCenter: 'datacenter1',
-//   keyspace:'system'
-// })
-
 const cassandra = require('cassandra-driver');
 const distance = cassandra.types.distance;
 
